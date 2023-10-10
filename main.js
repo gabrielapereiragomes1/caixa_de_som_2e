b73349c7b784edc9c6a9d2a63984aaa1937c1b25
gabrielapereiragomes1 <105368697+gabrielapereiragomes1@users.noreply.github.com>
2023-10-10 07:59:33 -0300

    ok

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom')
 }
 document.querySelector('.som_tecla_pom').onclick = tocaSomPom;
+
+function TocaSomClap(){
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_clap').onclick = TocaSomClap;
+
+const ListaDeTeclas = 
