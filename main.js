5dc10f5e664d296599b86d55fa17368a3d368305
gabrielapereiragomes1 <105368697+gabrielapereiragomes1@users.noreply.github.com>
2023-10-17 07:57:40 -0300

    ok

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 //atribuindo o clique no botão "0" que é o 1º item da lista de teclas
 listaDeTeclas[0].onclick = tocaSomPom;
-
+//criando referência várivael contador,atribuindo o valor inicial com "0"
+let contador = 0;
 //Criando o código de repetição WHILE
 while() {
 
